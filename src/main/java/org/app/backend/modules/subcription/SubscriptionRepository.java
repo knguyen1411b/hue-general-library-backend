248b180f5bfcd6d3048526ed5a73ecef6df03bc4
nhathuynguyen19 <huythcsthuyphuong73@gmail.com>
2026-04-04 07:32:20 +0700

    refactor: remove JpaSpecificationExecutor from SubscriptionRepository

--- src/main/java/org/app/backend/modules/subcription/SubscriptionRepository.java
+++ src/main/java/org/app/backend/modules/subcription/SubscriptionRepository.java
@@ -3,15 +3,15 @@ package org.app.backend.modules.subcription;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface SubscriptionRepository
-    extends JpaRepository<Subscription, UUID>, JpaSpecificationExecutor<Subscription> {
-  boolean existsByName(String name);
+    extends JpaRepository<Subscription, UUID>
+{
+    boolean existsByName(String name);
 
-  boolean existsByKey(String key);
+    boolean existsByKey(String key);
 
-  Optional<Subscription> findByKey(String key);
+    Optional<Subscription> findByKey(String key);
 
-  Optional<Subscription> findByName(String name);
+    Optional<Subscription> findByName(String name);
 }
